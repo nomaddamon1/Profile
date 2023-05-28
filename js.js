9c8b14312a04d5a568cebe4798cda4cda6682d8b
nomaddamon <31231166+nomaddamon1@users.noreply.github.com>
2023-05-28 18:28:58 +1200

    new JS making to do list work

--- js.js
+++ js.js
@@ -1,6 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react';
-
-const toDoInput = document.querySelector('inputField');
-
-const reset = document.querySelector('reset');
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent form submission
+  
+    var inputValue = document.querySelector('.todo-input').value.trim();
+    if (inputValue !== '') {
+      var listItem = document.createElement('li');
+      var textSpan = document.createElement('span');
+      textSpan.innerText = inputValue;
+      listItem.appendChild(textSpan);
+  
+      var deleteButton = document.createElement('button');
+      deleteButton.innerText = 'Delete';
+      deleteButton.addEventListener('click', function() {
+        listItem.remove();
+      });
+      listItem.appendChild(deleteButton);
+  
+      listItem.addEventListener('click', function() {
+        listItem.classList.toggle('completed');
+      });
+  
+      document.querySelector('.list').appendChild(listItem);
+      document.querySelector('.todo-input').value = '';
+    }
+  });
+  
